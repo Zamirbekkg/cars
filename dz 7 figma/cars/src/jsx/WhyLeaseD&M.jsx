aa6d4baf079a38ad0077fbdf854999293400eefa
Zamirbekkg <perizat9595kg78@gmail.com>
2024-09-17 12:19:41 +0600

    Add files via upload

---
+++ dz 7 figma/cars/src/jsx/WhyLeaseD&M.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+
+const WhyLeaseDM = () => {
+    return (
+    <header>
+      <div className='lease'>
+                <div className='top'>
+                    <div className='h12'>
+                    <h1>Why Lease with D&M?</h1>
+                </div>
+                <div className='top1'>
+                    <div className='icar'>
+                    
+                    </div>
+                    <h1>Top Dealers</h1>
+                    <h2>TrueCar Certified Dealers compete for your business by providing discounted pricing on in-stock inventory.</h2>
+                    </div>
+                </div>   
+
+                
+                <div className='top2'>
+                    <div className='icar2'>
+                        
+                    </div>
+                    <h1>Discounted Pricing</h1>
+                    <h2>View local inventory from the comfort of home and get access to guaranteed savings on the car of your choice.</h2>
+                </div>
+
+
+                <div className='top3'>
+                    <div className='icar3'>
+
+                    </div>
+                    <h1>Lower Payments</h1>
+                    <h2>Get discounted pricing when you shop for a car or truck using TrueCar and save on your lease payment</h2>
+                </div>
+
+                
+                <div className='top4'>
+                    <div icar4>
+
+                    </div>
+                    <h1>Easy Upgrade</h1>
+                    <h2>Easy to upgrade to the newest vehicle model every few years if you would like to be modern and comfortable</h2>
+                </div>
+         </div>
+        </header>
+    );
+}
+
+export default WhyLeaseDM;
